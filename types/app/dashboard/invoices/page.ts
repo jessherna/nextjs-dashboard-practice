cc28275e9c4742b5f7f0310b046fb4599e9eebea
Jessica Marie Hernandez <jessicamariep.hernandez@gmail.com>
2024-02-11 18:56:00 -0500

    removed .js from page in page.tsx

--- types/app/dashboard/invoices/page.ts
+++ types/app/dashboard/invoices/page.ts
@@ -1,8 +1,8 @@
 // File: C:\Users\jessi\OneDrive\Desktop\nextjs-dashboard\app\dashboard\invoices\page.tsx
-import * as entry from '../../../../../nextjs-dashboard/app/dashboard/invoices/page.js'
+import * as entry from '../../../../../nextjs-dashboard/app/dashboard/invoices/page'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../nextjs-dashboard/app/dashboard/invoices/page.js')
+type TEntry = typeof import('../../../../../nextjs-dashboard/app/dashboard/invoices/page')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
